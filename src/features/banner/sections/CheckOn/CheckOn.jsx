3d5c390b65277e7e1757657c9375210863f401dc
themandalorian <dwarfoffroad@protonmail.com>
2025-05-07 21:01:46 -0300

    refactor: cleaning code

--- src/features/banner/sections/CheckOn/CheckOn.jsx
+++ src/features/banner/sections/CheckOn/CheckOn.jsx
@@ -1,36 +1,36 @@
-import { HiOutlineViewGrid } from 'react-icons/hi'
+import { HiOutlineViewGrid } from "react-icons/hi";
 
 const CheckOn = ({ banner }) => {
   return (
-    <section className='banner__checkon'>
-      <h4 className='banner__checkon__title'>
+    <section className="banner__checkon">
+      <h4 className="banner__checkon__title">
         Confira <HiOutlineViewGrid />
       </h4>
-      <div className='banner__checkon__figures'>
+      <div className="banner__checkon__figures">
         {banner.banners.map((banner, index) => {
           return (
-            <figure className='banner__checkon__figure' key={index}>
-              <a href={banner.link} className='banner__checkon__anchor'>
+            <figure className="banner__checkon__figure" key={index}>
+              <a href={banner.link} className="banner__checkon__anchor">
                 <img
                   src={banner.url}
                   alt={`Banner ${banner.name}`}
-                  className='banner__checkon__img'
+                  className="banner__checkon__img"
                 />
-                <figcaption className='banner__checkon__info'>
-                  <span className='banner__checkon__freight'>
+                <figcaption className="banner__checkon__info">
+                  <span className="banner__checkon__freight">
                     {banner.name}
                   </span>
-                  <p className='banner__checkon__description'>
+                  <p className="banner__checkon__description">
                     {banner.description}
                   </p>
-                  <button className='banner__checkon__button'>Confira</button>
+                  <button className="banner__checkon__button">Confira</button>
                 </figcaption>
               </a>
             </figure>
-          )
+          );
         })}
         {/* <figure className="banner__checkon__figure">
-                    <img src="https://via.placeholder.com/600x300" alt="" className="banner__checkon__img" />
+                    <img src="https://picsum.photos/600/300" alt="" className="banner__checkon__img" />
                     <figcaption className="banner__checkon__info">
                         <span className="banner__checkon__freight">
                             Envios até 48 h
@@ -43,7 +43,7 @@ const CheckOn = ({ banner }) => {
                 </figure> */}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default CheckOn
+export default CheckOn;
--- src/features/banner/sections/Peak/Peak.jsx
+++ src/features/banner/sections/Peak/Peak.jsx
@@ -5,33 +5,35 @@
  */
 
 const Peak = ({ banner }) => {
-  console.log('fjsdiofjsd ', banner)
+  console.log("fjsdiofjsd ", banner);
 
   return (
-    <section className='banner__hightlight'>
-      <div className='banner__wrapper banner__wrapper--highlight'>
-        <ul className='banner__list banner__list--hightlight'>
+    <section className="banner__hightlight">
+      <div className="banner__wrapper banner__wrapper--highlight">
+        <ul className="banner__list banner__list--hightlight">
           {banner.banners.map((banner, key) => {
             return (
-              <li className='banner__item banner__item--hightlight' key={key}>
-                <a href='/a' className='banner__link banner__link--highlight'>
-                  <figure className=''>
+              <li className="banner__item banner__item--hightlight" key={key}>
+                <a href="/a" className="banner__link banner__link--highlight">
+                  <figure className="">
                     <img
-                      className='banner__image banner__image--hightlight'
+                      className="banner__image banner__image--hightlight"
                       src={banner.url}
                     />
-                    <legend className='banner__legand banner__legand--highlight'>
-                      {banner.name}
+                    <legend className="banner__legend banner__legend--highlight">
+                      {banner.name.split("").map((char, index) => (
+                        <span key={index}>{char}</span>
+                      ))}
                     </legend>
                   </figure>
                 </a>
               </li>
-            )
+            );
           })}
         </ul>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Peak
+export default Peak;
