0d5e2d99e37211acefbc14b07c923e43792995d9
themandalorian <dwarfoffroad@protonmail.com>
2025-05-21 10:14:23 -0300

    feat/characteristics_with_right_data

--- src/components/Product/Sections/ProductCharacteristics.jsx
+++ src/components/Product/Sections/ProductCharacteristics.jsx
@@ -1,35 +1,24 @@
 /**
- *
- * @returns
+ * Exibe as características do produto em forma de tabela
+ * @param {Array} characteristics - Lista de objetos com título e descrição
  */
 
-const ProductCharacteristics = ({ data }) => {
-  // console.log('Characte ', data)
+const ProductCharacteristics = ({ characteristics }) => {
+  if (!characteristics || characteristics.length === 0) return null;
+
   return (
-    <section className='product__allinfo' id='description'>
-      <h3 className='product__section'>
-        Conheça as caracteristicas
-      </h3>
-      <div className='product__table'>
-        <div className='product__row'>
-          <div className='product__td'>Alfred </div>
-          <div className='product__td'>Maria </div>
-        </div>
-        <div className='product__row'>
-          <div className='product__td'>Mexico </div>
-          <div className='product__td'>Tijuana </div>
-        </div>
-        <div className='product__row'>
-          <div className='product__td'>Mexico </div>
-          <div className='product__td'>Tijuana </div>
-        </div>
-        <div className='product__row'>
-          <div className='product__td'>Mexico </div>
-          <div className='product__td'>Tijuana </div>
-        </div>
+    <section className="product__allinfo" id="description">
+      <h3 className="product__section">Conheça as características</h3>
+      <div className="product__table">
+        {characteristics.map(({ id, title, description }) => (
+          <div className="product__row" key={id}>
+            <div className="product__td">{title}</div>
+            <div className="product__td">{description}</div>
+          </div>
+        ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default ProductCharacteristics
+export default ProductCharacteristics;
